8078b2ede18e2899b8dfaa511f739bad705ddb8d
BattulaLavanya <86053779+BattulaLavanya@users.noreply.github.com>
2021-07-21 21:47:22 +0530

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -1,5 +1,6 @@
 import pywhatkit
+ph_number = input()
 z=22
 while True:
     z=z+1
-    pywhatkit.sendwhatmsg('+917799999843', 'I Love You', 20, z)
+    pywhatkit.sendwhatmsg(ph_number, 'I Love You', 20, z)
